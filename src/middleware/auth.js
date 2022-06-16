75a45fee71920d64ddb4e8553a97273a3e003802
Sayanj4u <s.swarnakar96@gmail.com>
2022-06-16 15:27:40 +0530

    Assignment/auth-2

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,32 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const jwt= require("jsonwebtoken")
+const authenticate = function(req, res, next) {
+       //check the token in equest header
+       //validate this token
+       let token = req.headers["x-auth-token"]
+    if(!token) return res.send({status: false, msg: "token must be present in the request header"})
+    let decodedToken = jwt.verify(token, 'functionup-thorium')
 
-    next()
+    if(!decodedToken) return res.send({status: false, msg:"token is not valid"})
+    
+
+next()
+   
+}
+
+const authorise = function (req,res, next) {
+  
+  let token = req.headers["x-auth-token"]
+  let decodedToken = jwt.verify(token, 'functionup-thorium')
+  let userToBeModified = req.params.userId
+  //userId for the logged-in user
+  let userLoggedIn = decodedToken.userId
+
+  //
+  if(userToBeModified != userLoggedIn)
+  return res.send({status: true, msg: "user logged in is not allowed to modify the requested data"})
+  next()
 }
 
 
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    next()
-}
+module.exports.authenticate= authenticate;
+module.exports.authorise = authorise;
